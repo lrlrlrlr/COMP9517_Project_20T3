68152f594a8098d3bd0ed28bcce9d289e4e1adec
lrlrlrlr <acecardii@qq.com>
2020-11-12 14:04:54 +1100

    TASK2: hsv method

--- task2.py
+++ task2.py
@@ -4,19 +4,12 @@ from matplotlib import pyplot as plt
 import cv2
 import cv2 as cv
 import numpy as np
-
+import pandas, seaborn
+from collections import defaultdict
 
 # load the files
 ## Ara2012 - 16 files
 ## Ara2013 (Canon) - 27 files
-def get_files_path():
-    # ara2012_rgb = ['Tray/Ara2012/'+file for file in  os.listdir('Tray/Ara2012') if '_rgb.png' in file]
-    # ara2012_fg = ['Tray/Ara2012/'+file for file in  os.listdir('Tray/Ara2012') if '_fg.png' in file]
-    # ara2013_rgb = ['Tray/Ara2013-Canon/'+file for file in  os.listdir('Tray/Ara2012') if '_rgb.png' in file]
-    # ara2012_fg = ['Tray/Ara2012/'+file for file in  os.listdir('Tray/Ara2012') if '_fg.png' in file]
-    pass
-
-
 image = cv2.imread(r'D:\OneDrive - UNSW\COMP9331\COMP9517_Project_20T3\Tray\Ara2012\ara2012_tray01_rgb.png')
 image = cv2.resize(image, None, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
 
@@ -30,28 +23,62 @@ def attempt_hsv():
     # Convert BGR to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([28, 84, 50])
-    upper_blue = np.array([56, 255, 255])
+    # define range of green color in HSV
+    lower_green = np.array([28, 84, 50])
+    upper_green = np.array([56, 255, 255])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
+    mask = cv2.inRange(hsv, lower_green, upper_green)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(image, image, mask=mask)
-
-    # cv2.imshow('frame',frame)
-    # plt.imshow(mask)
-    # plt.show()
-    # plt.imshow(res)
-    # plt.show()
-
     return res
 
+
 def show_img(img, title=' '):
+    '''
+    simply show the image
+    :param img:
+    :param title:
+    :return:
+    '''
     plt.title(title)
     plt.imshow(img, cmap='gray')
     plt.show()
+
+## the most important function of this task!!!
+def count_pixels(arr, value=0) -> int:
+    '''
+    :param arr: flattened array of a single plant in the tray
+    :param value: 0 -- black,  255 -- white; default will count the white part of the images.
+    :return: INT
+    '''
+    return len([x for x in arr if x == value])
+
+
+#
+def analysis_threshold_of_medianblur(median2):
+    '''
+    plot the distribution of the
+    :param median2:
+    :return:
+    '''
+    hist = defaultdict(int)
+    for i in median2.flatten():
+        hist[i] += 1
+
+    df = pandas.DataFrame(hist.items())
+    df.columns = ['number', 'freq']
+
+    # remove the 0 's freq
+    df_new = df.query('number != 0')
+    df_new = df_new.set_index('number')
+
+    seaborn.histplot(df_new, x='number', y='freq')
+    plt.show()
+    seaborn.lineplot(data=df_new, x='number', y='freq')
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
@@ -60,31 +87,52 @@ if __name__ == '__main__':
     ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
 
-    show_img(thresh,'origin - threshed')
+    ## all the attempts to remove the noise (failed)
+    # show_img(gray,'origin - threshed')
+    # gau=cv2.GaussianBlur(gray,(5,5),0)
+    # show_img(gau, 'after gaussian')
+    #
+    # box = cv2.boxFilter(gray, -1, (3, 3), normalize=True)
+    # show_img(box, 'after box')
+    #
+    # double = cv2.bilateralFilter(gray, 5, 75, 75)
+    # show_img(double, 'after double')
 
 
 
-    gau=cv2.GaussianBlur(thresh,(5,5),0)
-    show_img(gau, 'after gaussian')
-
-    box = cv2.boxFilter(thresh, -1, (3, 3), normalize=True)
-    show_img(box, 'after box')
-
-    double = cv2.bilateralFilter(thresh, 5, 75, 75)
-    show_img(double, 'after double')
-
-    median = cv2.medianBlur(thresh, 3)
-    show_img(double, 'after median')
+    # naive method to remove the noise: by using medianBlur twice
+    median = cv2.medianBlur(gray, 3)
+    show_img(median, 'after median ')
+    median2 = cv2.medianBlur(median, 3)
+    show_img(median2, 'after median twice')
 
 
-    # put the img into a filter
+    # rethreshold the image after medianBlur
+    # 1. analysis the distribution of grayscale pixels
+    # analysis_threshold_of_medianblur(median2)
+    # 2. test the best threshold number
+    # for i in range(40,85,5):
+    #     ret, thresh2 = cv.threshold(median2, i, 255, cv.THRESH_BINARY)
+    #     show_img(thresh2, f'thresh2 - {i}')
+    #  -- 75 is the best
+
+    _, thresh2 = cv.threshold(median2, 75, 255, cv.THRESH_BINARY)
+    show_img(thresh2, f'thresh2 - 75')
+
+
+    # TODO: we assume we can get the borders of the plants (a,b,c,d), we need to
+    # get the array from a,b,c,d
+    # arr = GetFlattenArray(a,b,c,d, img)
+    # count_pixels(arr)
+
+    # counting the pixels:
+    img = cv.imread(r'D:\OneDrive - UNSW\COMP9331\COMP9517_Project_20T3\Tray\Ara2012\ara2012_tray01_fg.png', 0)
+    num_labels, labels_im = cv2.connectedComponents(img)
+
+    # we assume there is a plant in y=200~400, x = 500~1000.
+    crop_img = img[200:400, 500:1000]
+    show_img(crop_img)
+    print(f"pixel count: {count_pixels(crop_img.flatten())}")
 
 
 
-
-    show_img(thresh,'after thres')
-    # # noise removal
-    # kernel = np.ones((3, 3), np.uint8)
-    # opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
-
-    # counting the pixels of leaf area
